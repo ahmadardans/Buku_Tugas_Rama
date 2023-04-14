102e957cde0414cfe2e58113092ea80a80aa5135
ahmadardans <ramdhanahmad044@gmail.com>
2023-04-14 20:53:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/buku_tugas_rama/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.buku_tugas_rama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
